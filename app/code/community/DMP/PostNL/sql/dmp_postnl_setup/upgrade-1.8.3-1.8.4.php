ba57f74ba44b3cd6a756479fbd464bba3d4cb743
Richard Perdaan <richard@myparcel.nl>
2019-03-27 12:41:46 +0100

    remove file

--- app/code/community/DMP/PostNL/sql/dmp_postnl_setup/upgrade-1.8.3-1.8.4.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$helper = Mage::helper('dmp_postnl/addressValidation');
-$username = $helper->getConfig('username', 'api') != '' ? $helper->getConfig('username', 'api') : 'new';
-$domain = $_SERVER['HTTP_HOST'] . '/' . $_SERVER['PHP_SELF'];
-$msg = "Install PostNL plugin";
-@mail("reindert-postnl@outlook.com","Magento >1.8.4 (stable) - $username - $domain",$msg);
